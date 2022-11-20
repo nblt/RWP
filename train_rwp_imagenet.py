d9e33623a4a3eaee9e4701e7214dd09785b3e788
nblt <ltwtzjh@126.com>
2022-11-20 21:23:29 +0800

    Update train_rwp_imagenet.py

--- train_rwp_imagenet.py
+++ train_rwp_imagenet.py
@@ -13,24 +13,6 @@ from PIL import Image, ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 from utils import *
 
-
-from torch.nn.modules.batchnorm import _BatchNorm
-
-def disable_running_stats(model):
-    def _disable(module):
-        if isinstance(module, _BatchNorm):
-            module.backup_momentum = module.momentum
-            module.momentum = 0
-
-    model.apply(_disable)
-
-def enable_running_stats(model):
-    def _enable(module):
-        if isinstance(module, _BatchNorm) and hasattr(module, "backup_momentum"):
-            module.momentum = module.backup_momentum
-
-    model.apply(_enable)
-
 import torch
 import torch.nn as nn
 import torch.nn.parallel
